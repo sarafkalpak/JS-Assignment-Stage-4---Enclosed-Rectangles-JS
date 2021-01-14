be0903476c358f392575f32ccf45916243dd303f
sarafkalpak <sarafkalpak@gmail.com>
2021-01-14 22:11:02 +0530

    final

--- src/components/index.js
+++ src/components/index.js
@@ -8,70 +8,68 @@
 
 function updateStructure(rec1,rec2){
 	//write your code
-	if(contains(recA, recB)){
-		const relativeDim = relative(recA, recB);
-	return{...recA, children: [relativeDim]};
-	}else if(contains(recB, recA)){
-		const relativeDim = relative(recB, recA);
-		return{...recB, children:[relativeDim]};
+	if(contains(rec1, rec2)){
+		const relativeDim = relative(rec1, rec2);
+	return{...rec1, children: [relativeDim]};
+	}else if(contains(rec2, rec1)){
+		const relativeDim = relative(rec2, rec1);
+		return{...rec2, children:[relativeDim]};
 	}else{
-		return{...recA};
+		return{...rec1};
 	}
 }
 
-function relative(recA, recB){
-	const recAn = normalize(recA);
-	const recBn = normalize(recB);
+// function relative(rec1, rec2){
+// 	const recAn = normalize(rec1);
+// 	const recBn = normalize(rec2);
 
-	const res = {
-		children: recB.children
-	}
-	if(recB.top){
-		res.top = `${recBn.x1 - recAn.x1}px`;
-	}
-	if(recB.left){
-		res.left = `${recBn.y1 - recAn.y1}px`;
-	}
-	if(recB.height){
-		res.height = recB.height;
-	}
-	if(recB.width){
-		res.width = recB.width;
-	}
-	if(recB.bottom){
-		res.bottom = `${recAn.x2 - recBn.x2}px`;
-	}
-	if(recB.right){
-		res.right = `${recAn.y2 - recBn.y2}px`;
-	}
-	return res;
-}
+// 	const res = {
+// 		children: rec2.children
+// 	}
+// 	if(rec2.top){
+// 		res.top = `${recBn.x1 - recAn.x1}px`;
+// 	}
+// 	if(rec2.left){
+// 		res.left = `${recBn.y1 - recAn.y1}px`;
+// 	}
+// 	if(rec2.height){
+// 		res.height = rec2.height;
+// 	}
+// 	if(rec2.width){
+// 		res.width = rec2.width;
+// 	}
+// 	if(rec2.bottom){
+// 		res.bottom = `${recAn.x2 - recBn.x2}px`;
+// 	}
+// 	if(rec2.right){
+// 		res.right = `${recAn.y2 - recBn.y2}px`;
+// 	}
+// 	return res;
+// }
 
-function contains(recA, recB){
-	const recAn = normalize(recA);
-	const recBn = normalize(recB);
+// function contains(rec1, rec2){
+// 	const recAn = normalize(rec1);
+// 	const recBn = normalize(rec2);
 
-	if(
-		recAn.x1 <= recBn.x1
-		&& recAn.y1 <= recBn.y1
-		&& recAn.x2 >= recBn.x2
-		&& recAn.y2 >= recBn.y2
-	){
-		return true;
-	}
-	return false;
-}
-const T = 0;
-const W = 0;
-function normalize(rec){
-	return{
-		x1: rec.top ? parseInt(rec.top): (T-(parseInt(rec.bottom) + parseInt(rec.height))),
-		y1: rec.left ? parseInt(rec.left): (W-(parseInt(rec.right) + parseInt(rec.width))),
-		x2: rec.bottom ? (T-parseInt(rec.bottom)): (parseInt(rec.top)+ parseInt(rec.height)),
-		y2: rec.right ? (W-parseInt(rec.right)): (parseInt(rec.left)+ parseInt(rec.width))
-	}
-}
+// 	if(
+// 		recAn.x1 <= recBn.x1
+// 		&& recAn.y1 <= recBn.y1
+// 		&& recAn.x2 >= recBn.x2
+// 		&& recAn.y2 >= recBn.y2
+// 	){
+// 		return true;
+// 	}
+// 	return false;
+// }
+// const T = 0;
+// const W = 0;
+// function normalize(rec){
+// 	return{
+// 		x1: rec.top ? parseInt(rec.top): (T-(parseInt(rec.bottom) + parseInt(rec.height))),
+// 		y1: rec.left ? parseInt(rec.left): (W-(parseInt(rec.right) + parseInt(rec.width))),
+// 		x2: rec.bottom ? (T-parseInt(rec.bottom)): (parseInt(rec.top)+ parseInt(rec.height)),
+// 		y2: rec.right ? (W-parseInt(rec.right)): (parseInt(rec.left)+ parseInt(rec.width))
+// 	}
+// }
 
-
-module.exports = updateStructure;
 module.exports = updateStructure;
