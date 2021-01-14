f0c26f439826dbab637f00dcc6a1ecd08d36d8db
sarafkalpak <sarafkalpak@gmail.com>
2021-01-14 22:06:08 +0530

    final

--- src/components/index.js
+++ src/components/index.js
@@ -47,4 +47,31 @@ function relative(recA, recB){
 	return res;
 }
 
+function contains(recA, recB){
+	const recAn = normalize(recA);
+	const recBn = normalize(recB);
+
+	if(
+		recAn.x1 <= recBn.x1
+		&& recAn.y1 <= recBn.y1
+		&& recAn.x2 >= recBn.x2
+		&& recAn.y2 >= recBn.y2
+	){
+		return true;
+	}
+	return false;
+}
+const T = 0;
+const W = 0;
+function normalize(rec){
+	return{
+		x1: rec.top ? parseInt(rec.top): (T-(parseInt(rec.bottom) + parseInt(rec.height))),
+		y1: rec.left ? parseInt(rec.left): (W-(parseInt(rec.right) + parseInt(rec.width))),
+		x2: rec.bottom ? (T-parseInt(rec.bottom)): (parseInt(rec.top)+ parseInt(rec.height)),
+		y2: rec.right ? (W-parseInt(rec.right)): (parseInt(rec.left)+ parseInt(rec.width))
+	}
+}
+
+
+module.exports = updateStructure;
 module.exports = updateStructure;
